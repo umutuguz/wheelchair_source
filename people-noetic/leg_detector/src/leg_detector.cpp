178d3ea1e7bcdb8cb0bdcab441b38f30c00151a6
umutuguz <umutuguzlar7@gmail.com>
2022-08-18 15:20:53 +0300

    Umut

--- people-noetic/leg_detector/src/leg_detector.cpp
+++ people-noetic/leg_detector/src/leg_detector.cpp
@@ -65,7 +65,7 @@ static double max_second_leg_age_s     = 2.0;
 static double max_track_jump_m         = 1.0;
 static double max_meas_jump_m          = 0.75;  // 1.0
 static double leg_pair_separation_m    = 1.0;
-static const char* fixed_frame         = "odom_combined";
+static const char* fixed_frame         = "odom";
 
 static double kal_p = 4, kal_q = .002, kal_r = 10;
 static bool use_filter = true;
